7e653c5434b3294c9ac5f236f6e1bb41ccc63523
Theneras <90255538+Theneras@users.noreply.github.com>
2021-09-07 14:12:46 +0200

    Add files via upload

--- jd.php
+++ jd.php
@@ -1,12 +1,32 @@
 <?php
-$n = $_GET['war'];
-for($i=0; $i<$n; $i++){
-$los = rand(1, 1000);
-echo($los);
-echo("<br>");
+ if(isset($POST['oblicz'])){
+$log = $_POST['log'];
+$pass = $_POST['pass'];
+$a= $_POST['miasto'];
+$n = $_POST['nazwisko'];
+$c = $_POST['imie'];
+if($log=='jacek' && $pass == 'owca')
+{
+    echo("zalogowany");
+   echo("imie: " . $c .);
+   echo("nazwisko: " . $n .);
+   echo("miasto: " . $a .);
 }
+ else {
+    echo("makaron");
+ }
+
+
+ }
+
+
 ?>
-<form action=""method="GET">
-    <input type="text" name="war">
-    <input type="submit" value="show">
+<form action=""method="POST">
+   Login <input type="text" name="log">
+   Hasło <input type="password" name="pass">
+    <input type="submit" value="zaloguj">
+    <br>
+   Imie <input type="text" name="miasto">
+    nazwisko <input type="text" name="nazwisko">
+     miasto <input type="text"name="imie">
 </form>
